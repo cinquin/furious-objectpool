3e29f7d4c1c5b2b8af3e4f0153a7b8049d37af9b
eddie.raapoto@gmail.com <eddie.raapoto@gmail.com@619ba064-43e4-e763-ad49-a09234bbb621>
2012-01-06 12:52:06 +0000

    [maven-release-plugin]  copy for tag objectpool-1.0.1

---
+++ tags/objectpool-1.0.1/src/main/java/nf/fr/eraasoft/pool/impl/PoolFactory.java
@@ -0,0 +1,47 @@
+package nf.fr.eraasoft.pool.impl;
+
+import nf.fr.eraasoft.pool.ObjectPool;
+import nf.fr.eraasoft.pool.PoolSettings;
+import nf.fr.eraasoft.pool.PoolableObject;
+
+public class PoolFactory<T> {
+	final PoolSettings<T> settings;
+
+	AbstractPool<T> pool;
+	final PoolableObject<T> poolableObject;
+
+	public PoolFactory(PoolSettings<T> settings, PoolableObject<T> poolableObject) {
+		this.settings = settings;
+		this.poolableObject = poolableObject;
+	}
+
+	public ObjectPool<T> getPool() {
+		if (pool == null)
+			createPoolInstance();
+		return pool;
+	}
+
+	private static class BBObjectPool<T> extends BlockingQueueObjectPool<T> {
+
+		public BBObjectPool(PoolableObject<T> poolableObject, PoolSettings<T> settings) {
+			super(poolableObject, settings);
+
+		}
+
+	}
+
+	private synchronized void createPoolInstance() {
+		if (pool == null){
+			if (settings.max()>0)
+				pool = new BBObjectPool<T>(poolableObject, settings);
+			else 
+				pool = new ConcurrentLinkedQueuePool<T>(poolableObject, settings);
+		}
+
+	}
+
+	public PoolSettings<T> settings() {
+		return settings;
+	}
+
+}
